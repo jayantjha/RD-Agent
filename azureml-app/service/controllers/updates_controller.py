fd3f99e587551c8f8c119e65f99be5a2f36aa6d6
Jayant Kumar <kumarjayant@microsoft.com>
2025-04-11 03:14:16 +0530

    streaming

--- azureml-app/service/controllers/updates_controller.py
+++ azureml-app/service/controllers/updates_controller.py
@@ -9,7 +9,8 @@ async def event_stream():
     while True:
         await asyncio.sleep(2)
         guid = str(uuid.uuid4())
-        message = f"data: {{\"id\": \"{guid}\", \"message\": \"This is a stream update.\"}}\n\n"
+        
+        message = f"data: {{\"task\": \"CODING\", \"status\": \"STARTED\", \"createdAt\": 1744318179,  \"message\": \"Starting code implementation\"}}\n\n"
         yield message
 
 @router.get("/updates")
